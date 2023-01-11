c3e75413e75404fdbf1808dac7e1afaeae4be191
SumiAkter2 <akteralisha36@gmail.com>
2023-01-11 10:55:48 +0600

    nav fixed

--- src/Components/Home/Navbar.js
+++ src/Components/Home/Navbar.js
@@ -14,13 +14,11 @@ const Navbar = () => {
       <div className="flex justify-around items-center bg-transparent ">
         <div className="flex justify-center items-center">
           <img src={icon} alt="brand-icon" />
-          <h1 className="text-3xl font-bold text-black">Dhakru</h1>
+          <h1 className="text-3xl font-bold text-black">Bd Garage</h1>
         </div>
-        <p className="text-neutral lg:block hidden">
-          83/A Park Street Kolkata 700016
-        </p>
-        <p className="text-neutral lg:block hidden">contact@dhakru.com</p>
-        <p className="text-neutral lg:block hidden">Mon-Fri 08:00-18:00</p>
+        <p className="text-neutral lg:block hidden">Dhaka, Bangladesh</p>
+        <p className="text-neutral lg:block hidden">bdgarage@gmail.com</p>
+        <p className="text-neutral lg:block hidden">Mon-Fri 10:00-12:00</p>
         <div className="flex justify-around items-center text-neutral">
           <ImFacebook className="lg:mr-6 mr-2 lg:block hidden" />
           <BsTwitter className="lg:mr-6 mr-2 lg:block hidden" />
@@ -57,7 +55,7 @@ const Navbar = () => {
                 to="/eCommerce"
                 className="w-48 mx-auto mb-6 p-4 font-bold btn-secondary btn rounded-full btn-outline hover:text-white "
               >
-                <blink> Indian E-Commerce</blink>
+                <blink> BD E-Commerce</blink>
               </CustomLink>
             </ul>
           </div>
@@ -69,7 +67,7 @@ const Navbar = () => {
         <div className="navbar bg-accent text-white">
           <div className="navbar-start px-4">
             <button className="flex-2 btn btn-primary btn-outline bg-accent  rounded-full text-primary lg:hidden block">
-              Call us: +98786766
+              Call us: +880179999677
             </button>
 
             <ul className="menu menu-horizontal px-4  hidden lg:flex">
@@ -92,7 +90,7 @@ const Navbar = () => {
 
           <div className="navbar-end">
             <button className="flex-2 btn btn-primary btn-outline bg-accent  rounded-full text-primary lg:block hidden px-3">
-              Call us: +91 9284865249
+              Call us: +880179999675
             </button>
             <div className="flex lg:hidden justify-around items-center text-neutral px-3">
               <ImFacebook className="lg:mr-6 mr-2" />
